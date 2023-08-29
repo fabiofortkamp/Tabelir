478e27acccc8c6d2c4a51b77d7ba947ccbd3eda9
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2023-08-29 12:56:10 -0300

    Create functional test for checking output dir.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 """Test cases for the __main__ module."""
 
+from pathlib import Path
+
 import pytest
 from click.testing import CliRunner
 
@@ -16,3 +18,14 @@ def test_main_with_no_argument_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero. Default is methane."""
     result = runner.invoke(__main__.main)
     assert result.exit_code == 0
+
+
+def test_main_with_no_argument_writes_tables_to_current_dir(
+    runner: CliRunner, tmp_path
+) -> None:
+    """Running in a temporary dir should create a Methane folder."""
+    with runner.isolated_filesystem(temp_dir=tmp_path):
+        runner.invoke(__main__.main)
+
+        assert Path("Methane").exists()
+        assert Path("Methane").is_dir()
