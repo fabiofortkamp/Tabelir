57a5d750d0d5d30e4acc407701f1b5610a0746f5
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2023-08-31 11:16:10 -0300

    Create data structures and filenames

--- src/tabelir/__init__.py
+++ src/tabelir/__init__.py
@@ -1,5 +1,70 @@
 """Tabelir."""
+from enum import Enum
+
 from tabelir.app import TabelirApp
 
 
-__all__ = ["TabelirApp"]
+class SecondInput(Enum):
+    """A SecondInput is one of the possible second inputs to properties."""
+
+    ENTHALPY = "enthalpy"
+    TEMPERATURE = "temperature"
+
+    def __str__(self) -> str:
+        """Generate the string representation."""
+        return self.value
+
+
+class Phase(Enum):
+    """A Phase is one of the possible phases a fluid can be in."""
+
+    GAS = "GAS"
+    EQUIV_LIQUID = "equivLIQUID"
+    MIXTURE = "MIXTURE"
+
+    def __str__(self) -> str:
+        """Generate the string representation."""
+        return self.value
+
+
+class ThermophysicalProperty(Enum):
+    """A ThermophysicalProperty is one of the properties tabelir can calculate."""
+
+    DENSITY = "density"
+    CP = "cp"
+    EXPANSIVITY = "expansivity"
+    INTERFACIAL_TENSION = "interfacial_tension"
+    VISCOSITY = "viscosity"
+    CONDUCTIVITY = "conductivity"
+    ENTHALPY = "enthalpy"
+
+    def __str__(self) -> str:
+        """Generate the string representation."""
+        return self.value
+
+
+TABLE_EXTENSION = "csv"
+
+
+def filename_from(si: SecondInput, phase: Phase, prop: ThermophysicalProperty) -> str:
+    """Generate filename for the given case.
+
+    Args:
+        si (SecondInput): the second input (besides pressure) of the table
+        phase (Phase): phase of the fluid
+        prop (ThermophysicalProperty): property that is being calculated
+
+    Returns:
+        str: the formatted filename, ready to be created and written to
+    """
+    return f"{si}_{phase}_{prop}.{TABLE_EXTENSION}"
+
+
+__all__ = [
+    "TabelirApp",
+    "SecondInput",
+    "Phase",
+    "ThermophysicalProperty",
+    "filename_from",
+    "TABLE_EXTENSION",
+]
---
+++ tests/test_filenames.py
@@ -0,0 +1,57 @@
+"""test_filenames.py - Assert that filenames are well formed."""
+import itertools
+
+from tabelir import SecondInput, Phase, ThermophysicalProperty
+import tabelir
+
+
+def test_can_access_second_inputs() -> None:
+    """We can access the second inputs enumeration."""
+    values = list(SecondInput)
+    assert len(values) == 2
+
+    assert SecondInput.ENTHALPY in SecondInput
+    assert SecondInput.TEMPERATURE in SecondInput
+
+
+def test_can_access_phase() -> None:
+    """We can access all relevant phases."""
+    possible_phases = [Phase.GAS, Phase.EQUIV_LIQUID, Phase.MIXTURE]
+
+    assert set(possible_phases) == set(list(Phase))
+
+
+def test_can_access_property() -> None:
+    """We can access all relevant properties."""
+
+    possible_props = {
+        ThermophysicalProperty.DENSITY,
+        ThermophysicalProperty.CP,
+        ThermophysicalProperty.EXPANSIVITY,
+        ThermophysicalProperty.INTERFACIAL_TENSION,
+        ThermophysicalProperty.VISCOSITY,
+        ThermophysicalProperty.CONDUCTIVITY,
+        ThermophysicalProperty.ENTHALPY,
+    }
+
+    assert possible_props == set(list(ThermophysicalProperty))
+
+
+def test_can_generate_filename() -> None:
+    """Filenames are correctly formatted."""
+    phases = list(Phase)
+    second_inputs = list(SecondInput)
+    properties = list(ThermophysicalProperty)
+
+    for phase, si, prop in itertools.product(phases, second_inputs, properties):
+        filename = tabelir.filename_from(si, phase, prop)
+
+        assert filename == f"{si}_{phase}_{prop}.{tabelir.TABLE_EXTENSION}"
+
+    # let's also check an individual case
+    assert (
+        tabelir.filename_from(
+            SecondInput.ENTHALPY, Phase.EQUIV_LIQUID, ThermophysicalProperty.CP
+        )
+        == "enthalpy_equivLIQUID_cp.csv"
+    )
